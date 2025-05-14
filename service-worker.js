ef9988faca545eb37caffb79e33681118ea9bc3c
Dinesh Khaniya <dineshkhaniya001@gmail.com>
2025-05-14 12:00:02 +0530

    PWA

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,3 @@
-const CACHE_NAME = 'pwa-cache-v1';
-const urlsToCache = [
-    '/',
-    '/index.html',
-    '/manifest.json',
-    '/icons/icon-192.png',
-    '/src/index.js'
-];
-
-self.addEventListener('install', event => {
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(cache => cache.addAll(urlsToCache))
-    );
-});
-
-self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request).then(response => response || fetch(event.request))
-    );
+self.addEventListener('fetch', function (event) {
+    event.respondWith(fetch(event.request));
 });
--- src/index.js
+++
@@ -1,4 +0,0 @@
-console.log('🎮 Game logic starts here!');
-const canvas = document.getElementById('GameCanvas');
-canvas.style.backgroundColor = 'black';
-// Your game code can go here
