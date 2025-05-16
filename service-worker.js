e0fb3e885113981d7bcbef5ed69b6d6220c26a8c
Dinesh Khaniya <dineshkhaniya001@gmail.com>
2025-05-16 12:46:51 +0530

    PWA test

--- service-worker.js
+++ service-worker.js
@@ -1,14 +1,3 @@
 self.addEventListener('fetch', function (event) {
     event.respondWith(fetch(event.request));
 });
-window.addEventListener('REQ_BASE_URL', function () {
-    const baseUrl = import.meta.env.VITE_BASE_URL;
-    console.log("const baseUrl = import.meta.env.VITE_BASE_URL;", baseUrl);
-    const event = new CustomEvent("RES_BASE_URL", {
-        detail: {
-            platform: baseUrl
-        }
-    });
-    document.dispatchEvent(event);
-
-});
