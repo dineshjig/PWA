3411d1edf260fb2c95dc47a5a260e1dd4712baee
Dinesh Khaniya <dineshkhaniya001@gmail.com>
2025-05-27 18:45:14 +0530

    setup push notification

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,20 @@
 self.addEventListener('fetch', function (event) {
     event.respondWith(fetch(event.request));
 });
+self.addEventListener('push', function (event) {
+    const data = event.data?.json() || { title: "Default", body: "Push received" };
+
+    event.waitUntil(
+        self.registration.showNotification(data.title, {
+            body: data.body,
+            icon: 'icon-192.png'
+        })
+    );
+});
+
+self.addEventListener('notificationclick', function (event) {
+    event.notification.close();
+    event.waitUntil(
+        clients.openWindow('/')
+    );
+});
