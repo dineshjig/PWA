2eaf2bdb27343150eeae4cb629509dfe3f96a491
Dinesh Khaniya <dineshkhaniya001@gmail.com>
2025-05-27 11:08:55 +0530

    ui improvements and user avatar showing

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,22 @@
+importScripts('https://www.gstatic.com/firebasejs/10.4.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/10.4.0/firebase-messaging.js');
+
+firebase.initializeApp({
+    apiKey: "YOUR_API_KEY",
+    authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+    projectId: "YOUR_PROJECT_ID",
+    messagingSenderId: "YOUR_SENDER_ID",
+    appId: "YOUR_APP_ID",
+});
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage(function (payload) {
+    console.log('[firebase-messaging-sw.js] Received background message ', payload);
+    const notificationTitle = payload.notification.title;
+    const notificationOptions = {
+        body: payload.notification.body,
+    };
+
+    self.registration.showNotification(notificationTitle, notificationOptions);
+});
