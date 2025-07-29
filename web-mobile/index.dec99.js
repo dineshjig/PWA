f915a25037d6df79df6b54f776e37c803685b225
Dinesh Khaniya <Dinesh.Khaniya@globalvoxinc.com>
2025-07-29 18:26:46 +0530

    Texture and Asset Optimization

--- web-mobile/index.dec99.js
+++ web-mobile/index.dec99.js
@@ -1,4 +1,4 @@
-System.register(["./application.6ae3c.js"], function (_export, _context) {
+System.register(["./application.2c02a.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
