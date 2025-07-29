a6398fbbbc49278871e442d57a5e52ed7df4c8b5
Dinesh Khaniya <Dinesh.Khaniya@globalvoxinc.com>
2025-07-29 12:03:32 +0530

    Texture and Asset Optimization

--- web-mobile/application.2f5ce.js
+++ web-mobile/application.2f5ce.js
@@ -14,7 +14,7 @@ System.register([], function (_export, _context) {
       _export("Application", Application = /*#__PURE__*/function () {
         function Application() {
           _classCallCheck(this, Application);
-          this.settingsPath = 'src/settings.fa0cf.json';
+          this.settingsPath = 'src/settings.a6608.json';
           this.showFPS = false;
         }
         _createClass(Application, [{
--- web-mobile/index.521eb.js
+++ web-mobile/index.521eb.js
@@ -1,4 +1,4 @@
-System.register(["./application.f7bf4.js"], function (_export, _context) {
+System.register(["./application.2f5ce.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
