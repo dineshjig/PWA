0ed163447e50bdac0770267dd2838517ac92a2ac
Dinesh Khaniya <Dinesh.Khaniya@globalvoxinc.com>
2025-08-01 16:58:20 +0530

    create build for game testing

--- web-mobile/application.90c20.js
+++ web-mobile/application.90c20.js
@@ -14,7 +14,7 @@ System.register([], function (_export, _context) {
       _export("Application", Application = /*#__PURE__*/function () {
         function Application() {
           _classCallCheck(this, Application);
-          this.settingsPath = 'src/settings.97b1d.json';
+          this.settingsPath = 'src/settings.339f1.json';
           this.showFPS = false;
         }
         _createClass(Application, [{
--- web-mobile/index.25721.js
+++ web-mobile/index.25721.js
@@ -1,4 +1,4 @@
-System.register(["./application.2c02a.js"], function (_export, _context) {
+System.register(["./application.90c20.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
