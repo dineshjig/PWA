db0633abb637931232666d40b1569d7dd3cb907f
Dinesh Khaniya <Dinesh.Khaniya@globalvoxinc.com>
2025-08-06 11:14:34 +0530

    use compration

--- web-mobile/index.74c41.js
+++ web-mobile/index.74c41.js
@@ -1,4 +1,4 @@
-System.register(["./application.02dc0.js"], function (_export, _context) {
+System.register(["./application.a2c02.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
