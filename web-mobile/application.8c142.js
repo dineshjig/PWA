b2e7eaf2df9573a1981002557964f1f068ec8aee
Dinesh Khaniya <Dinesh.Khaniya@globalvoxinc.com>
2025-08-11 13:07:35 +0530

    use compration

--- web-mobile/application.8c142.js
+++ web-mobile/application.8c142.js
@@ -14,7 +14,7 @@ System.register([], function (_export, _context) {
       _export("Application", Application = /*#__PURE__*/function () {
         function Application() {
           _classCallCheck(this, Application);
-          this.settingsPath = 'src/settings.e8936.json';
+          this.settingsPath = 'src/settings.b8e6d.json';
           this.showFPS = false;
         }
         _createClass(Application, [{
