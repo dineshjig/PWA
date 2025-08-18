99ebcf2a8996eb97f04993f4bc62e73d80494a7d
Dinesh Khaniya <Dinesh.Khaniya@globalvoxinc.com>
2025-08-18 15:04:55 +0530

    use compration

--- web-mobile/index.3ea8e.js
+++ web-mobile/index.3ea8e.js
@@ -1,4 +1,4 @@
-System.register(["./application.52167.js"], function (_export, _context) {
+System.register(["./application.99c7d.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
