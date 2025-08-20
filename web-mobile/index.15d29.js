39fce35732a7569a102a95032f9922d9eec7f938
Dinesh Khaniya <Dinesh.Khaniya@globalvoxinc.com>
2025-08-20 11:59:26 +0530

    use orientationchange

--- web-mobile/index.15d29.js
+++ web-mobile/index.15d29.js
@@ -1,4 +1,4 @@
-System.register(["./application.93bb6.js"], function (_export, _context) {
+System.register(["./application.46e3d.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
