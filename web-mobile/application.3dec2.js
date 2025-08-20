4439879fb901ba23e4ddce909f92a03685158ddb
Dinesh Khaniya <Dinesh.Khaniya@globalvoxinc.com>
2025-08-20 11:02:17 +0530

    use orientationchange

--- web-mobile/application.3dec2.js
+++ web-mobile/application.3dec2.js
@@ -14,7 +14,7 @@ System.register([], function (_export, _context) {
       _export("Application", Application = /*#__PURE__*/function () {
         function Application() {
           _classCallCheck(this, Application);
-          this.settingsPath = 'src/settings.f3364.json';
+          this.settingsPath = 'src/settings.e3237.json';
           this.showFPS = false;
         }
         _createClass(Application, [{
--- web-mobile/index.b9e69.js
+++ web-mobile/index.b9e69.js
@@ -1,4 +1,4 @@
-System.register(["./application.165f2.js"], function (_export, _context) {
+System.register(["./application.3dec2.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
