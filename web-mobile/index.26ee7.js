99f0dc4120453bebd45a84b6019e91ea82ab240c
Dinesh Khaniya <Dinesh.Khaniya@globalvoxinc.com>
2025-09-10 10:27:17 +0530

    Create Firs Build For .live

--- web-mobile/index.26ee7.js
+++ web-mobile/index.26ee7.js
@@ -1,4 +1,4 @@
-System.register(["./application.ec731.js"], function (_export, _context) {
+System.register(["./application.34ea5.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
