ea1002a27bd96960e5b52ca14c574ac318c31fb5
Dinesh Khaniya <Dinesh.Khaniya@globalvoxinc.com>
2025-09-24 18:43:23 +0530

    create build for dev

--- web-mobile/index.5d8db.js
+++ web-mobile/index.5d8db.js
@@ -1,4 +1,4 @@
-System.register(["./application.50cec.js"], function (_export, _context) {
+System.register(["./application.0e9fb.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
