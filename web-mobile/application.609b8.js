4ee0b39d49012df362ca349a7318b422a836adfc
Dinesh Khaniya <Dinesh.Khaniya@globalvoxinc.com>
2025-09-24 13:52:04 +0530

    create build for dev

--- web-mobile/application.609b8.js
+++ web-mobile/application.609b8.js
@@ -14,7 +14,7 @@ System.register([], function (_export, _context) {
       _export("Application", Application = /*#__PURE__*/function () {
         function Application() {
           _classCallCheck(this, Application);
-          this.settingsPath = 'src/settings.16e79.json';
+          this.settingsPath = 'src/settings.02cf8.json';
           this.showFPS = false;
         }
         _createClass(Application, [{
--- web-mobile/index.60e86.js
+++ web-mobile/index.60e86.js
@@ -1,4 +1,4 @@
-System.register(["./application.026c4.js"], function (_export, _context) {
+System.register(["./application.609b8.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
