cfde269ff3b7f15314169f5dfc108d18bf3828e3
Dinesh Khaniya <Dinesh.Khaniya@globalvoxinc.com>
2025-09-24 14:56:48 +0530

    create build for dev

--- web-mobile/index.c86fc.js
+++ web-mobile/index.c86fc.js
@@ -1,4 +1,4 @@
-System.register(["./application.4c07f.js"], function (_export, _context) {
+System.register(["./application.50cec.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
