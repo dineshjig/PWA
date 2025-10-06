4807bcd4db3dd0192b6b8f592cc6fde431017b70
Dinesh Khaniya <Dinesh.Khaniya@globalvoxinc.com>
2025-10-06 19:17:25 +0530

    craet build

--- web-mobile/index.7d40d.js
+++ web-mobile/index.7d40d.js
@@ -1,4 +1,4 @@
-System.register(["./application.09c5b.js"], function (_export, _context) {
+System.register(["./application.62c39.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
