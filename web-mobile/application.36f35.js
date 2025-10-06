693839e1f918986ff52fdbba141926d299ddcca0
Dinesh Khaniya <Dinesh.Khaniya@globalvoxinc.com>
2025-10-06 14:29:08 +0530

    craet build

--- web-mobile/application.36f35.js
+++ web-mobile/application.36f35.js
@@ -14,7 +14,7 @@ System.register([], function (_export, _context) {
       _export("Application", Application = /*#__PURE__*/function () {
         function Application() {
           _classCallCheck(this, Application);
-          this.settingsPath = 'src/settings.d0b0b.json';
+          this.settingsPath = 'src/settings.fa585.json';
           this.showFPS = false;
         }
         _createClass(Application, [{
--- web-mobile/index.0935b.js
+++ web-mobile/index.0935b.js
@@ -1,4 +1,4 @@
-System.register(["./application.91c5e.js"], function (_export, _context) {
+System.register(["./application.36f35.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
